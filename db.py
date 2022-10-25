2b40aeb1763231f0f1a6a4fbecde5da22cb6d519
Diego1Michel <103611145+Diego1Michel@users.noreply.github.com>
2022-10-25 20:56:36 -0300

    master

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import pymysql
+import smtplib, ssl
+
 #conexão com o mySql
 connection=pymysql.connect(host="localhost", user = "root", passwd="00000000", database="testes", port=3306)
 cursor = connection.cursor()
@@ -14,3 +16,30 @@ def get_data():
     cursor.execute("SELECT * FROM form")
     rows = cursor.fetchall()
     return rows
+
+smtp = smtplib.SMTP_SSL('smtp.gmail.com', 587)
+email = 'topcars.t22@gmail.com'
+senha = 'top2022cars'
+
+# Abre o banco de dados
+db = pymysql.connect(host="localhost", user = "root", passwd="00000000", database="testes", port=3306)
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+sql = " SELECT ID, nome, email, cidade, endereco, telefone,data_nascimento FROM form"
+# executa o SQL
+cursor.execute(sql)
+
+# lista a base.
+results = cursor.fetchall()
+
+#loga no SMTP
+smtp.login(email,senha)
+
+for row in results:
+    id = row[0]
+    email_destino = row[2]
+    titulo = "Bem Vindo"
+    mensagem = "Obrigado por se cadastrar"
+   
